2eeef3aea00dec136c95869bb69a13c0d3a2a143
IEatSteak <vin24farrel@gmail.com>
2026-06-16 17:15:38 +0700

    cleaning unused code

--- TP_Util.java
+++ TP_Util.java
@@ -4,16 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 public class TP_Util {
-    public static double factorial(int n) {
-        double f = 1.0;
-
-        for (int i = 2; i <= n; i++) {
-            f *= i;
-        }
-
-        return f;
-    } 
-
     public static Map<Integer, Double> sort(Map<Integer, Double> scores) {
         List<Map.Entry<Integer, Double>> entries = new ArrayList<>(scores.entrySet());
 
@@ -26,9 +16,5 @@ public class TP_Util {
         }
 
         return result;
-    }
-
-    public static double poisson(int k, double lambda) {
-        return Math.exp(-lambda) * Math.pow(lambda, k) / factorial(k);
     }
 }
